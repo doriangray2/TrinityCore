67cc4f5e3f8427827dd6ca5a5d56caa54db17068
gorka.gonzalo@alumni.mondragon.edu <gorka.gonzalo@alumni.mondragon.edu>
2016-03-16 13:52:39 +0100

    Fixed Includes Fixed Format (Tabs to Spaces)

--- src/server/scripts/Draenor/BloodmaulSlagMines/boss_magmolatus.cpp
+++ src/server/scripts/Draenor/BloodmaulSlagMines/boss_magmolatus.cpp
@@ -15,113 +15,116 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+
 enum Spells
 {
-	SPELL_MAGMABARRAGE = 150004,
-	SPELL_MAGMABARRAGE_AOE = 150011,
-	SPELL_MOLTENBINDING = 149891,
-	SPELL_THROWEARTH = 150076,
-	SPELL_THROWFLAME = 150078,
+    SPELL_MAGMABARRAGE = 150004,
+    SPELL_MAGMABARRAGE_AOE = 150011,
+    SPELL_MOLTENBINDING = 149891,
+    SPELL_THROWEARTH = 150076,
+    SPELL_THROWFLAME = 150078,
 };
 
 enum Events
 {
-	EVENT_MAGMABARRAGE,
-	EVENT_MAGMABARRAGE_AOE,
-	EVENT_MOLTENBINDING,
-	EVENT_THROWEARTH,
-	EVENT_THROWFLAME,
+    EVENT_MAGMABARRAGE,
+    EVENT_MAGMABARRAGE_AOE,
+    EVENT_MOLTENBINDING,
+    EVENT_THROWEARTH,
+    EVENT_THROWFLAME,
 };
 
 class Boss_Magmolatus : public CreatureScript
 {
 public:
-	Boss_Magmolatus() : CreatureScript("boss_magmolatus") { }
+    Boss_Magmolatus() : CreatureScript("boss_magmolatus") { }
 
-	struct Boss_MagmolatusAI : public BossAI
-	{
-		Boss_MagmolatusAI(Creature* creature) : BossAI(creature, 74366)
-		{
-		}
+    struct Boss_MagmolatusAI : public BossAI
+    {
+        Boss_MagmolatusAI(Creature* creature) : BossAI(creature, 74366)
+        {
+        }
 
-		void Reset() override
-				{
-			BossAI::Reset();
-				}
+        void Reset() override
+                {
+            BossAI::Reset();
+                }
 
-		void EnterCombat(Unit* victim) override
-				{
-			BossAI::EnterCombat(victim);
+        void EnterCombat(Unit* victim) override
+                {
+            BossAI::EnterCombat(victim);
 
-			events.ScheduleEvent(EVENT_MAGMABARRAGE, 4000);
-			events.ScheduleEvent(EVENT_MAGMABARRAGE_AOE, 8000);
-			events.ScheduleEvent(EVENT_MOLTENBINDING, 12000);
-			events.ScheduleEvent(EVENT_THROWEARTH, 16000);
-			events.ScheduleEvent(EVENT_THROWFLAME, 20000);
-				}
+            events.ScheduleEvent(EVENT_MAGMABARRAGE, 4000);
+            events.ScheduleEvent(EVENT_MAGMABARRAGE_AOE, 8000);
+            events.ScheduleEvent(EVENT_MOLTENBINDING, 12000);
+            events.ScheduleEvent(EVENT_THROWEARTH, 16000);
+            events.ScheduleEvent(EVENT_THROWFLAME, 20000);
+                }
 
-		void KilledUnit(Unit * /*victim*/) override
-				{
-				}
+        void KilledUnit(Unit * /*victim*/) override
+                {
+                }
 
-		void JustDied(Unit * /*victim*/) override
-				{
-			_JustDied();
-				}
+        void JustDied(Unit * /*victim*/) override
+                {
+            _JustDied();
+                }
 
-		void UpdateAI(uint32 diff) override
-				{
-			if (!UpdateVictim())
-				return;
+        void UpdateAI(uint32 diff) override
+                {
+            if (!UpdateVictim())
+                return;
 
-			events.Update(diff);
+            events.Update(diff);
 
-			if (me->HasUnitState(UNIT_STATE_CASTING))
-				return;
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
 
-			while (uint32 eventId = events.ExecuteEvent())
-			{
-				switch (eventId)
-				{
-				case EVENT_MAGMABARRAGE:
-					if(Unit *target = SelectTarget(SELECT_TARGET_RANDOM, 0))
-						DoCast(target,SPELL_MAGMABARRAGE);
-					events.ScheduleEvent(EVENT_MAGMABARRAGE, 4000);
-					break;
-				case EVENT_MAGMABARRAGE_AOE:
-					DoCastAOE(SPELL_MAGMABARRAGE_AOE);
-					events.ScheduleEvent(EVENT_MAGMABARRAGE_AOE, 8000);
-					break;
-				case EVENT_MOLTENBINDING:
-					DoCastVictim(SPELL_MOLTENBINDING);
-					events.ScheduleEvent(EVENT_MOLTENBINDING, 12000);
-					break;
-				case EVENT_THROWEARTH:
-					DoCastVictim(SPELL_THROWEARTH);
-					events.ScheduleEvent(EVENT_THROWEARTH, 16000);
-					break;
-				case EVENT_THROWFLAME:
-					DoCastVictim(SPELL_THROWFLAME);
-					events.ScheduleEvent(EVENT_THROWFLAME, 20000);
-					break;
-				default:
-					break;
-				}
-			}
+            while (uint32 eventId = events.ExecuteEvent())
+            {
+                switch (eventId)
+                {
+                case EVENT_MAGMABARRAGE:
+                    if(Unit *target = SelectTarget(SELECT_TARGET_RANDOM, 0))
+                        DoCast(target,SPELL_MAGMABARRAGE);
+                    events.ScheduleEvent(EVENT_MAGMABARRAGE, 4000);
+                    break;
+                case EVENT_MAGMABARRAGE_AOE:
+                    DoCastAOE(SPELL_MAGMABARRAGE_AOE);
+                    events.ScheduleEvent(EVENT_MAGMABARRAGE_AOE, 8000);
+                    break;
+                case EVENT_MOLTENBINDING:
+                    DoCastVictim(SPELL_MOLTENBINDING);
+                    events.ScheduleEvent(EVENT_MOLTENBINDING, 12000);
+                    break;
+                case EVENT_THROWEARTH:
+                    DoCastVictim(SPELL_THROWEARTH);
+                    events.ScheduleEvent(EVENT_THROWEARTH, 16000);
+                    break;
+                case EVENT_THROWFLAME:
+                    DoCastVictim(SPELL_THROWFLAME);
+                    events.ScheduleEvent(EVENT_THROWFLAME, 20000);
+                    break;
+                default:
+                    break;
+                }
+            }
 
-			DoMeleeAttackIfReady();
-				}
-	};
+            DoMeleeAttackIfReady();
+                }
+    };
 
-	CreatureAI* GetAI(Creature* creature) const override
-			{
-		return new Boss_MagmolatusAI(creature);
-			}
+    CreatureAI* GetAI(Creature* creature) const override
+            {
+        return new Boss_MagmolatusAI(creature);
+            }
 };
 
 void AddSC_Boss_Magmolatus()
 {
-	new Boss_Magmolatus();
+    new Boss_Magmolatus();
 }
 
 
