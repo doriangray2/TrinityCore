5d5f97bcbb0ea0b77df173b69c99f801a3f62627
Infamous-devel <infamous.devel@gmail.com>
2016-04-07 11:49:20 +0200

    Added Pandaria Bosses: - Scarlet Monastery

--- src/server/scripts/Pandaria/pandaria_script_loader.cpp
+++ src/server/scripts/Pandaria/pandaria_script_loader.cpp
@@ -19,11 +19,20 @@ void AddSC_boss_armsmaster_harlan();
 void AddSC_boss_flameweaver_koegler();
 void AddSC_boss_houndmaster_braun();
 
+void AddSC_boss_brother_korloff();
+void AddSC_boss_high_inquisitor_whitemane();
+void AddSC_boss_thalnos_the_soulrender();
+
 void AddPandariaScripts()
 {
     /* Scarlet Halls */
     AddSC_boss_armsmaster_harlan();
     AddSC_boss_flameweaver_koegler();
     AddSC_boss_houndmaster_braun();
+
+    /* Scarlet Monastery */
+    AddSC_boss_brother_korloff();
+    AddSC_boss_high_inquisitor_whitemane();
+    AddSC_boss_thalnos_the_soulrender();
 
 }
